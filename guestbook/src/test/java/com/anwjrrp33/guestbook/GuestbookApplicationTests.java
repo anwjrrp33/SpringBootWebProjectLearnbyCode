e4920fd457b8a50dbdf19eac43bc5a2b80546027
anwjrrp33 <anwjrrp33@gmail.com>
2021-01-25 23:44:15 +0900

    guestbook

---
+++ guestbook/src/test/java/com/anwjrrp33/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anwjrrp33.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
